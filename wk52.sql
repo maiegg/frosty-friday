b6bafd0d3857d15bc555f2c389582800b9034ade
maiegg <maggieowilson@gmail.com>
2025-05-07 12:11:37 -0700

    wip

---
+++ wk52.sql
@@ -0,0 +1,77 @@
+
+CREATE OR REPLACE PROCEDURE check_and_generate_data(tableName STRING)
+RETURNS STRING
+LANGUAGE JAVASCRIPT
+EXECUTE AS CALLER
+AS
+$$
+try {
+var tableName = TABLENAME;
+
+var command1 = `SELECT count(*) as count FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = '${tableName.toUpperCase()}'`;
+var statement1 = snowflake.createStatement({sqlText: command1});
+var result_set1 = statement1.execute();
+
+result_set1.next();
+var count = result_set1.getColumnValue('COUNT');
+
+if (count == 0) {
+    var command2 = `CREATE TABLE ${tableName} (payload VARIANT, ingested_at TIMESTAMP_NTZ default CURRENT_TIMESTAMP())`;
+    var statement2 = snowflake.createStatement({sqlText: command2});
+    statement2.execute();
+
+    return `Table ${tableName} has been created.`;
+} else {
+    for(var i=0; i<40; i++) {
+        var jsonObject = {
+            "id": i,
+            "name": "Name_" + i,
+            "address": "Address_" + i,
+            "email": "email_" + i + "@example.com",
+            "transactionValue": Math.floor(Math.random() * 10000) + 1
+        };
+        var jsonString = JSON.stringify(jsonObject);
+
+        var command3 = `INSERT INTO ${tableName} (payload) SELECT PARSE_JSON(column1) FROM VALUES ('${jsonString}')`;
+        var statement3 = snowflake.createStatement({sqlText: command3});
+        statement3.execute();
+    }
+
+    return `40 records have been inserted into the ${tableName} table.`;
+}
+
+} catch (err) {
+return "Failed: " + err;
+}
+$$;
+
+create or replace dynamic table week52_clean 
+  target_lag = '1 minute'
+  warehouse = COMPUTE_WH
+as 
+select payload:address::text as address
+, payload:email::text as email
+, payload:id::number as id
+, payload:transactionValue::int as transaction_value
+, payload:name::text as name 
+, ingested_at
+from week52
+;
+
+
+call check_and_generate_data('week52'); -- first run creates table
+call check_and_generate_data('week52'); -- second adds 40 rows 
+
+select count(*) from week52; -- just to check = 40 rows 
+
+-- manually refresh DT 
+alter dynamic table week52_clean refresh;
+select count(*) from week52_clean; -- Check, expect 40 rows 
+
+call check_and_generate_data('week52'); -- added 40 more rows at 12:10
+select count(*) from week52; -- check, expect 80 rows 
+
+-- wait a few minutes 
+select count(*) from week52_clean; -- check expect 80 rows - Success
+
+drop dynamic table week52_clean; -- to avoid wasting compute 
